dacb30f29d3225f10e8ef681a7444e9945ebe92a
tmartin314 <troy@devatotech.com>
2018-04-08 23:33:09 -0400

    added ui tweaks to main navbar

--- app/presenters/onboard/project_presenter.rb
+++ app/presenters/onboard/project_presenter.rb
@@ -1,15 +1,4 @@
 class Onboard::ProjectPresenter < Rectify::Presenter
   attribute :user, User
 
-  def organization
-    user.organizations.first.name.titleize
-  end
-
-  def organization_id
-    user.organizations.first.id
-  end
-
-  def organization_slug
-    user.organizations.first.slug
-  end
 end
