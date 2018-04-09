220e994710873a4dff59db81afd7377510b02357
tmartin314 <troy@devatotech.com>
2018-04-08 21:43:14 -0400

    rubocop

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,7 +4,7 @@ class Organization < ApplicationRecord
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-  has_many :user_organizations
+  has_many :user_organizations, dependent: :destroy
   has_many :users, through: :user_organizations
   has_many :projects
 
