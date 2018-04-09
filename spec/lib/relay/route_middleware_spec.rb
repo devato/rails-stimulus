1804df9a2a648d19816797bd245a034ae02709f4
tmartin314 <troy@devatotech.com>
2018-04-08 21:26:34 -0400

    clean up spec

--- spec/lib/relay/route_middleware_spec.rb
+++ spec/lib/relay/route_middleware_spec.rb
@@ -22,46 +22,37 @@ end
 
 describe Relay::RouteMiddleware, type: :middleware do
 
-  let(:app) { MockRackApp.new }
+  let(:app)     { MockRackApp.new }
   let(:request) { Rack::MockRequest.new(subject) }
   let!(:organization) { create(:organization, name: 'findme') }
 
   subject { described_class.new(app) }
 
   context 'when no org is found in the request' do
-
     before do
       request.get('/notanorg', 'REQUEST_PATH' => '/notanorg')
     end
-
     it 'does not set the current org' do
       expect(Current.organization).to eq(nil)
     end
-
   end
 
   context 'when org is found in REQUEST_PATH' do
-
     before do
       request.get('/findme', 'REQUEST_PATH' => '/findme')
     end
-
     it 'sets the current org' do
       expect(Current.organization).to eq(organization)
     end
-
   end
 
   context 'when org is found in HTTP_REQUEST_PATH' do
-
     before do
       request.get('/findme', 'HTTP_REQUEST_PATH' => '/findme')
     end
-
     it 'sets the current org' do
       expect(Current.organization).to eq(organization)
     end
-
   end
 
 end
